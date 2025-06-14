82f3680e02120581450f8e98cee9e0f4a7f7a923
Gabriel Hartmann <162393857+GabHartmann@users.noreply.github.com>
2025-06-13 22:23:12 -0300

    Swagger endpoints documented
    Added the necessary documentation of the endpoints using swagger.

--- app.js
+++ app.js
@@ -8,7 +8,14 @@ const taskRouter = require("./src/routes/private/byDomain/taskRoutes.js");
 const userRouter = require("./src/routes/private/byDomain/userRoutes.js");
 const loginRouter = require("./src/routes/public/loginRoute.js");
 
+const { swaggerUi, swaggerDocument } = require("./src/config/swagger");
+
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(cors());
 app.use(express.json());
